8126e2a799b4df515651b833fe15adb5a1a29373
Naren <narendas10@gmail.com>
2023-03-11 13:14:01 +0530

    Folderstructure_Commit

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path: str) -> List[str]:
 
 setup(
     name="mlproject_demo",
-    version="0.0.2",
+    version="0.0.3",
     author="Naren",
     author_email="narendas10@gmail.com",
     install_requires=get_requirements("requirements.txt"),
